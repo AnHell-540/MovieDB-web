78f47a04853819622b27140f401d94a39651999d
AnHell-540 <angel.martinez@540deg.com>
2024-06-11 11:32:26 +0200

    Repositorio para traer detalle de películas

---
+++ src/core/infrastructure/MovieDetailRepository.ts
@@ -0,0 +1,28 @@
+import { MovieResult } from "../domain/Movies.interface";
+
+export const MovieDetailRepository = () => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+    },
+  };
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+  const fetchMovieDetail = async (id:string): Promise<MovieResult> => {
+    try {
+      const data = await fetch(
+        `${baseUrl}${id}?language=en-US`,
+        options
+      );
+      const response: MovieResult = await data.json();
+      return response;
+    } catch (e) {
+      console.error(e);
+      throw new Error("Error fetch.");
+    }
+  };
+
+  return { fetchMovieDetail };
+};
