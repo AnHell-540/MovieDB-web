6dbebb6091afbf471a414606fde68f4a3aad1ec0
AnHell-540 <angel.martinez@540deg.com>
2024-07-15 12:24:22 +0200

    Refactor con nuevos componentes Solución a posición del scroll al entrar a una película

--- src/ui/views/MovieDetail/Movie.tsx
+++ src/ui/views/MovieDetail/Movie.tsx
@@ -1,74 +1,38 @@
 import style from "./Movie.module.css";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useLocation } from "react-router-dom";
 import { useGetMovieDetail } from "../../customHooks";
 import { MovieDetailRepository } from "../../../core/infrastructure";
 import { MovieDetailService } from "../../../core/usecase";
-import { CinemaMap, MovieRating } from "../../components";
+import { CinemaMap } from "../../components";
+import { useEffect } from "react";
+import { ImageAndTitle } from "./ImageAndTitle";
+import { MovieInfo } from "./MovieInfo";
 
 export const Movie = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, [location]);
+
   const movieDetailRepository = MovieDetailRepository();
   const movieDetailService = MovieDetailService(movieDetailRepository);
 
-  const imgUrl = "https://image.tmdb.org/t/p/w1280";
   const [params] = useSearchParams();
   const movieId = params.get("id") ?? "";
 
   const { movie } = useGetMovieDetail(() =>
     movieDetailService.getMovieDetail(movieId)
   );
-  const genreString = movie.genres.map((g) => g.name).join(", ");
 
   return (
     <>
       <div className={style.container}>
-        <div className={style.header}>
-          <div className={style.header_img_container}>
-            <img
-              className={style.header_image}
-              src={`${imgUrl}${movie.backdrop_path}`}
-              alt="header_img"
-            />
-          </div>
-          <div className={style.title}>
-            <p>TMDB / Movie details</p>
-            <h3>{movie.title}</h3>
-          </div>
-        </div>
-
-        <main className={style.movie_info_container}>
-          <figure className={style.poster_container}>
-            <img
-              className={style.poster}
-              src={process.env.REACT_APP_POSTER_IMAGE + movie.poster_path}
-              alt="poster_img"
-            />
-          </figure>
-
-          <section className={style.movie_info}>
-            <h4>{movie.tagline}</h4>
-            <p className={style.overview}>{movie.overview}</p>
-            <MovieRating
-              movie={movie}
-              classContainer={style.rating}
-              classValue={style.rating_value}
-            />
-            <div>
-              <p className={style.info_section_title}>Release Date:</p>
-              <p className={style.info_section_content}>{movie.release_date}</p>
-            </div>
-            <div>
-              <p className={style.info_section_title}>Run time</p>
-              <p className={style.info_section_content}>{movie.runtime}</p>
-            </div>
-            <div>
-              <p className={style.info_section_title}>Genres</p>
-              <p className={style.info_section_content}>{genreString}</p>
-            </div>
-          </section>
-        </main>
+        <ImageAndTitle movie={movie} />
+        <MovieInfo movie={movie} />
       </div>
       <section className={style.map_container}>
-        <h3 className={style.map_title}>Localización de cines cercanos</h3>
+        <h3 role="map" className={style.map_title}>Localización de cines cercanos</h3>
         <CinemaMap />
       </section>
     </>
