3ff85714c479d9efc7aa11987939fcadfcc08546
AnHell-540 <angel.martinez@540deg.com>
2024-08-01 09:43:17 +0200

    Corrección de ancho máximo en NavBar Autoformateo de NavBar Corrección de logo duplicado y cambio de nombre index y NavBar actualizados con el nuevo nombre del logo

--- src/ui/components/NavBar/NavBar.tsx
+++ src/ui/components/NavBar/NavBar.tsx
@@ -1,42 +1,59 @@
 import { Link, NavLink } from "react-router-dom";
-import style from './NavBar.module.css'
-import logo from '../../../assets/images/logo-navbar.png'
+import style from "./NavBar.module.css";
+import logo from "../../../assets/images/logo.png";
 
 export const NavBar = () => {
-
   return (
     <div className={style.header}>
       <nav className={style.navbar}>
-        
         <div className={style.navbar_div_logo}>
           <Link className={style.link_logo} to="/">
-            <img className={style.logo} src={logo} alt="home navigation logo"  />
+            <img className={style.logo} src={logo} alt="home navigation logo" />
           </Link>
         </div>
 
         <ul className={style.navbar_links}>
-          <li> 
-          <NavLink className={({ isActive }) => isActive ? style.active_navbar_link : style.navbar_link} to="/">
-            Popular
-          </NavLink>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? style.active_navbar_link : style.navbar_link
+              }
+              to="/"
+            >
+              Popular
+            </NavLink>
           </li>
-          <li> 
-          <NavLink className={({ isActive }) => isActive ? style.active_navbar_link : style.navbar_link} to="/top-rated">
-            Top Rated
-          </NavLink>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? style.active_navbar_link : style.navbar_link
+              }
+              to="/top-rated"
+            >
+              Top Rated
+            </NavLink>
           </li>
-          <li> 
-          <NavLink className={({ isActive }) => isActive ? style.active_navbar_link : style.navbar_link} to="/favorites">
-            Favorites
-          </NavLink>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? style.active_navbar_link : style.navbar_link
+              }
+              to="/favorites"
+            >
+              Favorites
+            </NavLink>
           </li>
-          <li> 
-          <NavLink className={({ isActive }) => isActive ? style.active_navbar_link : style.navbar_link} to="/next-releases">
-            Next Releases
-          </NavLink>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? style.active_navbar_link : style.navbar_link
+              }
+              to="/next-releases"
+            >
+              Next Releases
+            </NavLink>
           </li>
         </ul>
-        
       </nav>
     </div>
   );
